440a9de76e5f9a86d2d298df4d2f0977c346e376
NikhilWaghmode09 <nick13waghmode@gmail.com>
2024-02-20 14:20:13 +0530

    BaseWizard

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import ERC1155 from './ERC1155.jsx'
 import './App.css'; // Import your CSS file for styling
 
 const App = () => {
-  const [activeFileType, setActiveFileType] = useState('ERC20');
-
-  const selectERC20 = () => {
-    setActiveFileType('ERC20');
-  };
+  const [activeFileType, setActiveFileType] = useState('ERC721');
 
   const selectERC721 = () => {
     setActiveFileType('ERC721');
@@ -17,14 +13,12 @@ const App = () => {
   const selectERC1155 = () => {
     setActiveFileType('ERC1155');
   };
-
+  const selectERC20 = () => {
+    setActiveFileType('ERC20');
+  };
   return (
     <div>
-      {/* Button for ERC20 */}
-      <button className="toggle-button" onClick={selectERC20}>
-        ERC20
-      </button>
-
+      
       {/* Button for ERC721 */}
       <button className="toggle-button" onClick={selectERC721}>
         ERC721
@@ -33,11 +27,15 @@ const App = () => {
       <button className="toggle-button" onClick={selectERC1155}>
         ERC1155
       </button>
-
+      {/* Button for ERC20 */}
+      <button className="toggle-button" onClick={selectERC20}>
+        ERC20
+      </button>
       {/* Render different components based on active file type */}
-      {activeFileType === 'ERC20' && <ERC20 />}
+      
       {activeFileType === 'ERC721' && <ERC721 />}
       {activeFileType === 'ERC1155' && <ERC1155 />}
+      {activeFileType === 'ERC20' && <ERC20 />}
     </div>
   );
 };
