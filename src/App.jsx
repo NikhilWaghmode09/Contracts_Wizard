138fe2dde9cfbbef8775a8411b2724bc8473e2e0
yashjawale <yash25.j@gmail.com>
2024-04-28 20:30:10 +0530

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -9,59 +9,7 @@ import ERC4907 from './ERC4907.jsx'
 import './App.css'
 
 const App = () => {
-  const [activeFileType, setActiveFileType] = useState('ERC721')
-
-  const selectERC721 = () => {
-    setActiveFileType('ERC721')
-  }
-  const selectERC1155 = () => {
-    setActiveFileType('ERC1155')
-  }
-  const selectERC20 = () => {
-    setActiveFileType('ERC20')
-  }
-  const selectERC721A = () => {
-    setActiveFileType('ERC721A')
-  }
-  const selectERC404 = () => {
-    setActiveFileType('ERC404')
-  }
-  const selectERC4907 = () => {
-    setActiveFileType('ERC4907')
-  }
-  return (
-    <div>
-      {/* Button for ERC721 */}
-      <button className="toggle-button" onClick={selectERC721}>
-        ERC721
-      </button>
-      {/* Button for ERC1155 */}
-      <button className="toggle-button" onClick={selectERC1155}>
-        ERC1155
-      </button>
-      {/* Button for ERC20 */}
-      <button className="toggle-button" onClick={selectERC20}>
-        ERC20
-      </button>
-      <button className="toggle-button" onClick={selectERC721A}>
-        ERC721A
-      </button>
-      <button className="toggle-button" onClick={selectERC404}>
-        ERC404
-      </button>
-      <button className="toggle-button" onClick={selectERC4907}>
-        ERC4907
-      </button>
-      {/* Render different components based on active file type */}
-
-      {activeFileType === 'ERC721' && <ERC721 />}
-      {activeFileType === 'ERC1155' && <ERC1155 />}
-      {activeFileType === 'ERC20' && <ERC20 />}
-      {activeFileType === 'ERC721A' && <ERC721A />}
-      {activeFileType === 'ERC404' && <ERC404 />}
-      {activeFileType === 'ERC4907' && <ERC4907 />}
-    </div>
-  )
+  return <h1>Hello</h1>
 }
 
 export default App
