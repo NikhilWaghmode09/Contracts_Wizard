f1697dee8cc383607f3e10d302d56656712f4f87
yashjawale <yash25.j@gmail.com>
2024-04-28 20:21:34 +0530

    Add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import React, { useState } from 'react';
-import ERC20 from './ERC20.jsx';
-import ERC721 from './ERC721.jsx';
-import ERC1155 from './ERC1155.jsx';
-import ERC721A from './ERC721A.jsx';
-import ERC404 from './ERC404.jsx';
-import ERC4907 from './ERC4907.jsx';
+import React, { useState } from 'react'
+import ERC20 from './ERC20.jsx'
+import ERC721 from './ERC721.jsx'
+import ERC1155 from './ERC1155.jsx'
+import ERC721A from './ERC721A.jsx'
+import ERC404 from './ERC404.jsx'
+import ERC4907 from './ERC4907.jsx'
 
-import './App.css';
+import './App.css'
 
 const App = () => {
-  const [activeFileType, setActiveFileType] = useState('ERC721');
+  const [activeFileType, setActiveFileType] = useState('ERC721')
 
   const selectERC721 = () => {
-    setActiveFileType('ERC721');
-  };
+    setActiveFileType('ERC721')
+  }
   const selectERC1155 = () => {
-    setActiveFileType('ERC1155');
-  };
+    setActiveFileType('ERC1155')
+  }
   const selectERC20 = () => {
-    setActiveFileType('ERC20');
-  };
+    setActiveFileType('ERC20')
+  }
   const selectERC721A = () => {
-    setActiveFileType('ERC721A');
-  };
+    setActiveFileType('ERC721A')
+  }
   const selectERC404 = () => {
     setActiveFileType('ERC404')
-  };
+  }
   const selectERC4907 = () => {
     setActiveFileType('ERC4907')
-  };
+  }
   return (
     <div>
-      
       {/* Button for ERC721 */}
       <button className="toggle-button" onClick={selectERC721}>
         ERC721
@@ -54,16 +53,15 @@ const App = () => {
         ERC4907
       </button>
       {/* Render different components based on active file type */}
-      
+
       {activeFileType === 'ERC721' && <ERC721 />}
       {activeFileType === 'ERC1155' && <ERC1155 />}
       {activeFileType === 'ERC20' && <ERC20 />}
       {activeFileType === 'ERC721A' && <ERC721A />}
       {activeFileType === 'ERC404' && <ERC404 />}
       {activeFileType === 'ERC4907' && <ERC4907 />}
-
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
