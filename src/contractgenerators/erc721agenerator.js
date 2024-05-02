ebac03533b5a3852d084a22bf10e863e8ce7f943
yashjawale <yash25.j@gmail.com>
2024-05-02 09:25:06 +0530

    Complete 721a generator function

--- src/contractgenerators/erc721agenerator.js
+++ src/contractgenerators/erc721agenerator.js
@@ -1,5 +1,31 @@
 const getErc721aString = ({ name, symbol, mintable, burnable, queryable }) => {
-  return `TODO: Implement ERC721A Generator`
+
+  let mintableimport = mintable ? `\nimport '@openzeppelin/contracts/access/Ownable.sol';` : '';
+  let burnableimport = burnable ? `\nimport 'erc721a/contracts/extensions/ERC721ABurnable.sol';` : '';
+  let queryableimport = queryable ? `\nimport 'erc721a/contracts/extensions/ERC721AQueryable.sol';` : '';
+
+  let mintableinherit = mintable ? ', Ownable' : '';
+  let burnableinherit = burnable ? ', ERC721ABurnable' : '';
+  let queryableinherit = queryable ? ', ERC721AQueryable' : '';
+
+  let mintableconstructor = mintable ? 'Ownable(initialOwner)' : '';
+  let mintableparameter = mintable ? 'address initialOwner' : '';
+  let mintablefunction = mintable ? `
+  function safeMint(address to, uint256 quantity) public onlyOwner { 
+    _safeMint(to, quantity, '');
+  }` : '';
+
+  return `// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+  
+import 'erc721a/contracts/ERC721A.sol';${mintableimport}${burnableimport}${queryableimport}
+
+contract ${name} is ERC721A ${mintableinherit} ${burnableinherit} ${queryableinherit}{
+  constructor(${mintableparameter}) ERC721A ("${name}","${symbol}") ${mintableconstructor} 
+  {}
+  ${mintablefunction}
+}
+  `;
 }
 
 const erc721agenerator = ({ name, symbol, mintable, burnable, queryable }) => {
