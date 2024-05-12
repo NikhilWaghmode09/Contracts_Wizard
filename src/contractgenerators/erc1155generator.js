aa21f35fc6c19b52ce9f52b034e0472ebf2fe769
Nikhil Sunil Waghmode <nick13waghmode@gmail.com>
2024-05-12 16:34:31 +0530

    manual render updates to erc20, erc721 & erc1155

--- src/contractgenerators/erc1155generator.js
+++ src/contractgenerators/erc1155generator.js
@@ -1,8 +1,65 @@
-import { erc1155 } from "@openzeppelin/wizard"
+const getErc1155String = ({ name, uri, mintable, burnable, pausable, upgradableuri, supply }) => {
+  let ownableimport = (mintable || pausable || upgradableuri) ? `\nimport "@openzeppelin/contracts/access/Ownable.sol";` : '';
 
+  let ownableinherit = (mintable || pausable || upgradableuri) ? `, Ownable` : '';
+
+  let ownableparameter = (mintable || pausable || upgradableuri) ? `address initialOwner` : '';
+  let ownableconstructor = (mintable || pausable || upgradableuri) ? `Ownable(initialOwner)` : '';
+  let mintable_fun = mintable ? `
+  function mint(address account, uint256 id, uint256 amount, bytes memory data)
+    public
+    onlyOwner
+  {
+    _mint(account, id, amount, data);
+  }
+  function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    public
+    onlyOwner
+  {
+    _mintBatch(to, ids, amounts, data);
+  }` : '';
+  let supplyinherit = supply ? `, ERC1155Supply` : '';
+  let burnableimport = burnable ? `\nimport "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";` : '';
+  let burnableinherit = burnable ? `, ERC1155Burnable` : '';
+
+  let pausableimport = pausable ? `\nimport "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";` : '';
+  let pausableinherit = pausable ? `, ERC1155Pausable` : '';
+  let pausable_fun = pausable ? `
+  function pause() public onlyOwner {
+    _pause();
+  }\n
+  function unpause() public onlyOwner {
+    _unpause();
+  }` : '';
+  let update_fun = (pausable || supply) ? `
+  function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+    internal
+    override(ERC1155${pausableinherit}${supplyinherit})
+  {
+    super._update(from, to, ids, values);
+  }` : '';
+
+  let upgradeable_fun = upgradableuri ? `
+  function setURI(string memory newuri) public onlyOwner {
+    _setURI(newuri);
+  }` : '';
+  let supplyimport = supply ? `\nimport "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";` : '';
+  
+
+
+  return `// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+  
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";${ownableimport}${pausableimport}${burnableimport}${supplyimport}
+  
+contract ${name} is ERC1155${ownableinherit}${pausableinherit}${burnableinherit}${supplyinherit} {
+  constructor(${ownableparameter}) ERC1155("${uri}")${ownableconstructor} {}
+  ${upgradeable_fun}${pausable_fun}${mintable_fun}${update_fun}
+}`;
+}
 
 const erc1155generator = ({ name, uri, mintable, burnable, pausable, upgradableuri, supply }) => {
-  return erc1155.print(
+  return getErc1155String(
     {
       name: name || 'ExampleToken',
       uri: uri || '',
--- src/contractgenerators/erc721generator.js
+++ src/contractgenerators/erc721generator.js
@@ -1,104 +1,113 @@
 const getErc721String = ({ name, symbol, mintable, burnable, pausable, votes, baseuri, uristorage, enumerable, incremental }) => {
-  let ownableimport = (mintable || pausable) ? `import "@openzeppelin/contracts/access/Ownable.sol";` : '';
-  let ownableinherit = (mintable || pausable) ? `, Ownable` : '';
-  let ownableparameter = (mintable || pausable) ? `address initialOwner` : '';
-  let ownableconstructor = mintable ? `Ownable(initialOwner)` : '';
-  let mintablefunction = (mintable && !incremental) ? `
-  function safeMint(address to, uint256 tokenId) public onlyOwner {
-    _safeMint(to, tokenId);
-  }` : '';
+  
+  //import statements
+  let enumerableimport = enumerable ? `\nimport "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";` : '';
+  let uristorageimport = uristorage ? `\nimport "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; ` : '';
+  let pausableimport = pausable ? `\nimport "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";` : '';
+  let ownableimport = (mintable || pausable) ? `\nimport "@openzeppelin/contracts/access/Ownable.sol";` : '';
+  let burnableimport = burnable ? `\nimport "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol"; ` : '';
+  let voteimport = votes ? `\nimport "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";` : '';
 
-  let incrementaldeclaration = (incremental && mintable) ? `uint256 private _nextTokenId;` : '';
-  let incrementalfunction = (mintable && incremental) ? `
-  function safeMint(address to) public onlyOwner {
-    uint256 tokenId = _nextTokenId++;
-    _safeMint(to, tokenId);
-  }` : '';
-
-  let baseUrifunction = baseuri ? `function _baseURI() internal pure override returns (string memory) {
-    return "${baseuri}";
-}` : '';
-
-  let burnableimport = burnable ? `import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol"; ` : '';
-  let burnableinherit = burnable ? `, ERC721Burnable` : '';
-
-  let pausableimport = pausable ? `import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";` : '';
+  //inherit statements
+  let enumerableinherit = enumerable ? `, ERC721Enumerable` : '';
+  let uristorageinherit = uristorage ? `, ERC721URIStorage` : '';
   let pausableinherit = pausable ? `, ERC721Pausable` : '';
-  let pausablefunction = pausable ? `
+  let ownableinherit = (mintable || pausable) ? `, Ownable` : '';
+  let burnableinherit = burnable ? `, ERC721Burnable` : '';
+  let eipinherit = votes ? `, EIP712` : '';
+  let voteinherit = votes ? `, ERC721Votes` : '';
+
+  //incremental declaration and defination
+  let incrementaldeclaration = (incremental && mintable) ? `\n  uint256 private _nextTokenId;` : '';
+  let incrementaldefinition = (mintable && incremental) ? `\n    uint256 tokenId = _nextTokenId++;` : '';
+  
+  //parameters
+  let mintparameter = (mintable && !incremental) ? `, uint256 tokenId` : '';
+  let uriparameter = uristorage ?  `, string memory uri` : '';
+  let ownableparameter = (mintable || pausable) ? `address initialOwner` : '';
+
+  let urifunctioncall = uristorage ? `\n    _setTokenURI(tokenId, uri);` : '';
+
+  //constructors
+  let ownableconstructor = (mintable || pausable) ? `
+    Ownable(initialOwner)` : '';
+  let voteconstructor = votes ? `
+    EIP712("ExampleToken", "1")` : '';
+
+  //functions
+  let pausable_fun = pausable ? `
   function pause() public onlyOwner {
     _pause();
-  }
-
+  }\n
   function unpause() public onlyOwner {
     _unpause();
-  }
+  }\n` : '';
 
-  // The following functions are overrides required by Solidity.
-
-  function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Pausable) returns (address) {
+  let mintable_fun = (mintable || uristorage) ? `
+  function safeMint(address to${mintparameter}${uriparameter}) public onlyOwner {${incrementaldefinition}
+    _safeMint(to, tokenId);${urifunctioncall}
+  }\n` : '';
+  
+  let update_fun = (enumerable || pausable || votes) ? `
+  function _update(address to, uint256 tokenId, address auth) 
+    internal 
+    override(ERC721${enumerableinherit}${pausableinherit}${voteinherit}) 
+    returns (address) 
+  {
     return super._update(to, tokenId, auth);
+  }\n`: '';
+
+  let baseuri_fun = baseuri ? `\n
+  function _baseURI() 
+    internal 
+    pure 
+    override 
+    returns (string memory) 
+  {
+    return "${baseuri}";
   }` : '';
 
-  let voteimport = votes ? `import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";` : '';
-  let voteinherit = votes ? `, EIP712, ERC721Votes` : '';
-  let voteconstructor = votes ? `EIP712("ExampleToken", "1")` : '';
-  let votefunction = votes ? `// The following functions are overrides required by Solidity.
-
-  function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Votes) returns (address) {
-    return super._update(to, tokenId, auth);
-  }
-
-  function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Votes) {
+  let vote_enumerable_fun = (votes || enumerable) ? `
+  function _increaseBalance(address account, uint128 value) 
+    internal 
+    override(ERC721${enumerableinherit}${voteinherit}) 
+  {
     super._increaseBalance(account, value);
-  }` : '';
-
-  let uristorageimport = uristorage ? `import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; ` : '';
-  let uristorageinherit = uristorage ? `, ERC721URIStorage` : '';
-  let uristoragefunction = uristorage ? `
-  function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-  return super.tokenURI(tokenId);
-  }
-
-  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
-  return super.supportsInterface(interfaceId);
-  }` : '';
-
-  let enumerableimport = enumerable ? `import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";` : '';
-  let enumerableinherit = enumerable ? `, ERC721Enumerable` : '';
-  let enumerablefunction = enumerable ? `
-  // The following functions are overrides required by Solidity.
-
-  function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
-    return super._update(to, tokenId, auth);
-  }
-
-  function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
-    super._increaseBalance(account, value);
-  }
-
-  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+  }\n` : '';
+  
+  let uristorage_fun = uristorage  ? `
+  function tokenURI(uint256 tokenId) 
+    public 
+    view 
+    override(ERC721, ERC721URIStorage) 
+    returns (string memory) 
+  {
+    return super.tokenURI(tokenId);
+  }\n` : '';
+  
+  let enumerable_uristorage_fun = (enumerable || uristorage) ? `
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721${enumerableinherit}${uristorageinherit})
+    returns (bool)
+  {
     return super.supportsInterface(interfaceId);
   }` : '';
+
 
   return `// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
   
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-${ownableimport}
-${burnableimport}
-${pausableimport}
-${voteimport}
-${uristorageimport}
-${enumerableimport}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";${enumerableimport}${uristorageimport}${pausableimport}${ownableimport}${burnableimport}${voteimport}
   
-contract ${name} is ERC721${ownableinherit}${burnableinherit}${pausableinherit}${voteinherit}${uristorageinherit}${enumerableinherit} {
-  ${incrementaldeclaration}
-  constructor(${ownableparameter}) ERC721("${name}", "${symbol}") ${ownableconstructor}${voteconstructor}{}
-  ${mintablefunction}${pausablefunction}${votefunction}${uristoragefunction}${enumerablefunction}${incrementalfunction}${baseUrifunction}
-}
-
-  `;
+contract ${name} is ERC721${enumerableinherit}${uristorageinherit}${pausableinherit}${ownableinherit}${burnableinherit}${eipinherit}${voteinherit} {${incrementaldeclaration}
+  constructor(${ownableparameter}) 
+    ERC721("${name}", "${symbol}") ${ownableconstructor} ${voteconstructor} 
+  {}
+  ${pausable_fun} ${mintable_fun} ${update_fun} ${vote_enumerable_fun} ${uristorage_fun} ${enumerable_uristorage_fun} ${baseuri_fun}
+}`;
 }
 
 const erc721generator = ({ name, symbol, mintable, burnable, pausable, votes, baseuri, uristorage, enumerable, incremental }) => {
@@ -111,7 +120,7 @@ const erc721generator = ({ name, symbol, mintable, burnable, pausable, votes, ba
       burnable: burnable || false,
       pausable: pausable || false,
       votes: votes || false,
-      uriStorage: uristorage || null,
+      uristorage: uristorage || null,
       enumerable: enumerable || false,
       incremental: incremental || false
     }
